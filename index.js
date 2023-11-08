9e79cb92055379cc465f34d9588d9d6e5e94ea01
Anshul Sharma <114267698+anshulsharma1610@users.noreply.github.com>
2023-11-07 21:44:34 -0500

    Test (#15)
    * added route 53 config
    
    * userdata fix
    
    * changed env
    
    * logger changes

--- index.js
+++ index.js
@@ -301,7 +301,7 @@ const createSubnetsAndEC2 = async () => {
 
     // Specify the database configuration
     const dbHostname = pulumi.interpolate`${rdsInstance.address}`;
-
+    const NODE_ENV = 'production';
     // User data script to configure the EC2 instance
     const userDataScript = pulumi.interpolate`#!/bin/bash
     echo "MYSQL_DATABASE=${dbName}" >> /opt/csye6225/.env
@@ -310,11 +310,19 @@ const createSubnetsAndEC2 = async () => {
     echo "MYSQL_HOST=${dbHostname}" >> /opt/csye6225/.env
     echo "METRICS_HOSTNAME=${metricsHostname}" >> /opt/csye6225/.env
     echo "METRICS_PORT=${metricsPort}" >> /opt/csye6225/.env
+    echo "NODE_ENV=${NODE_ENV}" >> /opt/csye6225/.env
+    echo "REGION=${region}" >> /opt/csye6225/.env
     
     # Start the CloudWatch Agent and enable it to start on boot
     sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/csye6225/api/cloudwatch/config.json -s
+    
+    # Enable and start the systemd service
+    sudo systemctl daemon-reload
     sudo systemctl enable amazon-cloudwatch-agent
     sudo systemctl start amazon-cloudwatch-agent
+
+    sudo systemctl enable cloud.service
+    sudo systemctl start cloud.service
     `;
     
     // pulumi.log.info(
