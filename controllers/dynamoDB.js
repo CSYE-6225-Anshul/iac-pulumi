927855f87971b4a49bee93dc7a3db1bc1fbd519c
Anshul Sharma <sharma.ansh@northeastern.edu>
2023-11-28 18:14:16 -0500

    alter dynamo db table

--- controllers/dynamoDB.js
+++ controllers/dynamoDB.js
@@ -1,32 +1,69 @@
 const aws = require('@pulumi/aws');
+const uuid = require('uuid');
 
 const createDynamoDB = () => {
     // DynamoDB Table
     return dynamoTable = new aws.dynamodb.Table("mytable", {
         attributes: [
-            { name: "Email", type: "S" },  // Email column
-            { name: "URL", type: "S" },    // URL column
-            { name: "EmailSentTime", type: "S" },  // Email sent time column
+            { name: "id", type: "S" },  // Unique identifier with UUID
+            { name: "email", type: "S" },
+            { name: "submissionURL", type: "S" },
+            { name: "gcsURL", type: "S" },
+            { name: "emailSentTime", type: "S" },
+            { name: "assignmentId", type: "S" },
+            { name: "accountId", type: "S" },
         ],
-        hashKey: "Email", // Using Email as the hash key
+        hashKey: "id", // Using Email as the hash key
         readCapacity: 1,
         writeCapacity: 1,
         globalSecondaryIndexes: [
             {
-                name: "EmailSentTimeIndex",
-                hashKey: "EmailSentTime",
+                name: "EmailIndex",
+                hashKey: "email",
                 projectionType: "ALL",
                 readCapacity: 1,
                 writeCapacity: 1,
             },
             {
-                name: "URLIndex",
-                hashKey: "URL",
+                name: "SubmissionUrlIndex",
+                hashKey: "submissionURL",
                 projectionType: "ALL",
                 readCapacity: 1,
                 writeCapacity: 1,
-            }
+            },
+            {
+                name: "GcsUrlIndex",
+                hashKey: "gcsURL",
+                projectionType: "ALL",
+                readCapacity: 1,
+                writeCapacity: 1,
+            },
+            {
+                name: "EmailSentTimeIndex",
+                hashKey: "emailSentTime",
+                projectionType: "ALL",
+                readCapacity: 1,
+                writeCapacity: 1,
+            },
+            {
+                name: "AssignmentIdIndex",
+                hashKey: "assignmentId",
+                projectionType: "ALL",
+                readCapacity: 1,
+                writeCapacity: 1,
+            },
+            {
+                name: "AccountIdIndex",
+                hashKey: "accountId",
+                projectionType: "ALL",
+                readCapacity: 1,
+                writeCapacity: 1,
+            },
         ],
+    }, {
+        dynamicProviders: {
+            id: () => uuid.v4(),
+        },
     });
 }
 
